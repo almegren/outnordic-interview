27d331344a948b5f590d70d46aa579d4b26926b2
Simon Opheim <simon.opheim@fjellsport.no>
2025-04-04 13:01:19 +0200

    chore: bump deps

--- order-history/project.clj
+++ order-history/project.clj
@@ -3,10 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.github.seancorfield/next.jdbc "1.3.834"]
-                 [org.postgresql/postgresql "42.2.10"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.1002"]
+                 [org.postgresql/postgresql "42.7.5"]]
   :main ^:skip-aot order-history.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
